e2b144428865b73a565a55e5bbc27a8563d814b0
hotdl <huang26169@gmail.com>
2019-01-23 00:05:47 +0800

    0001,0002

--- src/com/hotdl/leetcode/lt0001_two_sum/Solution.java
+++ src/com/hotdl/leetcode/lt0001_two_sum/Solution.java
@@ -1,10 +1,11 @@
-package com.hotdl.leetcode.lt0001_twosum;
+package com.hotdl.leetcode.lt0001_two_sum;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 〈〉
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  *
  * @author Hotdl
  * @date 2019/1/22 22:56
